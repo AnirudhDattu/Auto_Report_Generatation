44ed514a295d42114d64599777ba8e114e590388
Anirudh Dattu <112719357+AnirudhDattu@users.noreply.github.com>
2025-12-07 14:48:14 +0530

    logo changes

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,11 @@
-
-import React from 'react';
+import React from "react";
 
 export const Logo: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center w-24 h-24 relative">
-      <img 
-        src="images/logo.png" 
-        alt="AGS Logo" 
+      <img
+        src="images/logo.png"
+        alt="AGS Logo"
         className="w-full h-full object-contain"
         crossOrigin="anonymous"
       />
--- components/Signature.tsx
+++ components/Signature.tsx
@@ -1,12 +1,12 @@
-
-import React from 'react';
+import React from "react";
+// import LOGO from "../images/logo.png";
 
 export const Signature: React.FC = () => {
   return (
     <div className="h-full w-full flex items-center justify-start">
-      <img 
-        src="images/signature.png" 
-        alt="Signature" 
+      <img
+        src="images/signature.png"
+        alt="Signature"
         className="h-full w-auto object-contain"
         crossOrigin="anonymous"
       />
